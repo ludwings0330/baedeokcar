5d5f38cda3f9e94e6d9f6255b2c5c6ae8574bebc
ludwings <ludwings0330@gmail.com>
2021-11-23 21:51:08 +0900

    Initial commit

---
+++ src/main/java/com/project/baedeokcar/BaedeokcarApplication.java
@@ -0,0 +1,13 @@
+package com.project.baedeokcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedeokcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaedeokcarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/baedeokcar/BaedeokcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.baedeokcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedeokcarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
