ff528e88fad671042789498c06218305cab60e7b
KDH <ejrgus2956@naver.com>
2021-11-23 22:16:35 +0900

    야야야 받아랏

--- src/main/java/com/project/baedeokcar/controller/helloController.java
+++ src/main/java/com/project/baedeokcar/controller/helloController.java
@@ -1,4 +1,20 @@
 package com.project.baedeokcar.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
 public class helloController {
+
+    @GetMapping("/")
+    public String index(){
+
+        log.info("index 가나요? >ㅡ< ");
+
+        return "index";
+
+    }
+
 }
