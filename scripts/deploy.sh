a6f27103d2f3d39a3d6ce35643a1c9233f041167
ludwings <ludwings0330@gmail.com>
2021-12-19 23:15:36 +0900

    test: 배포 자동화 구성 테스트
    deploy.sh 코드 수정. CURRENT_PID 부분 수정
    
    #2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,8 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME}.*.jar)
+CURRENT_PID=$(pgrep -fl baedeokcar | grep jar | awk '{print $1}')
+
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
@@ -21,7 +22,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
