45025a6738d320688dedc0971bf354f7d1163157
ludwings <ludwings0330@gmail.com>
2021-12-21 20:42:23 +0900

    feat: AWS S3 File Upload 기능 구현
    aws ec2의 역할을 추가해서 ec2에서 직접 s3로 접근가능하도록,
    access key 와 secret key 를 ec2에서 자동으로 발급받아 해당 내용을 사용하도록 적용
    
    #2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,6 +32,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-real-db.yml,/home/ec2-user/app/application-aws.yml \
+        -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-real-db.yml \
         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
